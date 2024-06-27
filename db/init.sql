d84825f0b7512c7eb073788689a13e9ee35d6c5f
Andrey <andreypomortsev@gmail.com>
2024-06-27 17:56:23 +0500

    Создал файл инициализации базы данных

---
+++ db/init.sql
@@ -0,0 +1,35 @@
+-- Создаем таблицу authors
+CREATE TABLE authors (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    middle_name VARCHAR(100),
+    birth_year INTEGER NOT NULL
+);
+
+-- Создаем таблицу books
+CREATE TABLE books (
+    bookId SERIAL PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    author_id INTEGER REFERENCES authors(id),
+    year INTEGER,
+    status BOOLEAN NOT NULL DEFAULT TRUE
+);
+
+-- Создаем таблицу users
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    middle_name VARCHAR(100),
+    birth_year INTEGER
+);
+
+-- Создаем таблицу loans
+CREATE TABLE loans (
+    id SERIAL PRIMARY KEY,
+    book_id INTEGER REFERENCES books(bookId) NOT NULL,
+    user_id INTEGER REFERENCES users(id) NOT NULL,
+    loan_date DATE NOT NULL DEFAULT CURRENT_DATE,
+    return_date DATE DEFAULT NULL
+);
