9f9de6fd2e5df69c5598a0b6622107bad7af1002
Andrey <andreypomortsev@gmail.com>
2024-06-28 10:27:24 +0500

    Изменил имя файла

---
+++ app/schemas/author.py
@@ -0,0 +1,24 @@
+from pydantic import BaseModel
+from typing import Optional
+
+class AuthorCreate(BaseModel):
+    name: str
+    last_name: str
+    middle_name: Optional[str] = None
+    birth_year: int
+
+class AuthorUpdate(BaseModel):
+    name: Optional[str] = None
+    last_name: Optional[str] = None
+    middle_name: Optional[str] = None
+    birth_year: Optional[int] = None
+
+class Author(BaseModel):
+    id: int
+    name: str
+    last_name: str
+    middle_name: Optional[str] = None
+    birth_year: int
+
+    class Config:
+        orm_mode = True
