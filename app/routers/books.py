316cf16d616acdafab95a681607af76358b0354d
Andrey <andreypomortsev@gmail.com>
2024-06-28 17:56:44 +0500

    Fixed create_book

--- app/routers/books.py
+++ app/routers/books.py
@@ -2,22 +2,15 @@ from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from schemas.book import BookCreate, BookUpdate, Book
 from db.models import Book as DBBook
-from db.database import get_db, get_author_id_by_name_last_name
+from db.database import get_db
 
 router = APIRouter()
 
 
 @router.post("/books/create", response_model=Book)
 def create_book(book: BookCreate, db: Session = Depends(get_db)):
-    # Получение ID автора используя storing procedure
-    author_id = get_author_id_by_name_last_name(
-        db, book.author_name, book.author_last_name, book.author_birth_year
-    )
-    if not author_id:
-        raise HTTPException(status_code=404, detail="Автор не найден")
-
     # Создаем книгу с полученным ID автора
-    db_book = DBBook(title=book.title, author_id=author_id, year=book.year)
+    db_book = DBBook(**book.dict())
     db.add(db_book)
     db.commit()
     db.refresh(db_book)
