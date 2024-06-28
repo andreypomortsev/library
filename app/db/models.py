2cd87cc9d399e5361ae01bec8237f5bd2375389f
Andrey <andreypomortsev@gmail.com>
2024-06-28 13:49:39 +0500

    Добавил черновик класса books

--- app/db/models.py
+++ app/db/models.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
+from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
@@ -12,3 +13,15 @@ class Author(Base):
     last_name = Column(String, index=True)
     middle_name = Column(String, nullable=True)
     birth_year = Column(Integer)
+
+class Book(Base):
+    __tablename__ = "books"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String(255), nullable=False)
+    author_id = Column(Integer, ForeignKey('authors.id'))
+    year = Column(Integer)
+    status = Column(Boolean, nullable=False, default=True)
+
+    # Определяем отношение к автору
+    author = relationship("Author", back_populates="books")
