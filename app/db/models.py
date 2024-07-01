069500913dbc762566c25eb634e0de8578b067c8
Andrey <andreypomortsev@gmail.com>
2024-07-01 17:13:50 +0500

    Added class Loan, and links between the classes

--- app/db/models.py
+++ app/db/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey, Date
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -15,17 +15,20 @@ class Customer(Base):
     birth_year = Column(Integer, nullable=False)
     is_author = Column(Boolean, nullable=False, default=False)
 
+    join = "and_(Customer.id == foreign(Book.author_id), \
+        Customer.is_author == True)"
     books = relationship(
         "Book",
         back_populates="author",
-        primaryjoin="and_(Customer.id == foreign(Book.author_id), Customer.is_author == True)",
+        primaryjoin=join,
     )
+    loans = relationship("Loan", back_populates="user")
 
 
 class Book(Base):
     __tablename__ = "books"
 
-    id = Column(Integer, primary_key=True, autoincrement=True)
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     title = Column(String(255), nullable=False)
     author_id = Column(Integer, ForeignKey("customers.id"))
     genre = Column(String(255), nullable=False)
@@ -33,3 +36,17 @@ class Book(Base):
     status = Column(Boolean, nullable=False, default=True)
 
     author = relationship("Customer", back_populates="books")
+    loans = relationship("Loan", back_populates="book")
+
+
+class Loan(Base):
+    __tablename__ = "loans"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    book_id = Column(Integer, ForeignKey("books.id"), index=True, nullable=False)
+    user_id = Column(Integer, ForeignKey("customers.id"), nullable=False)
+    loan_date = Column(Date, nullable=False, index=True, default="now()")
+    return_date = Column(Date, nullable=True, default=None)
+
+    book = relationship("Book", back_populates="loans")
+    user = relationship("Customer", back_populates="loans")
