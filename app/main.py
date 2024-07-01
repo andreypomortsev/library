4cfbc6f96da9a181716e20ae867dd8b57a480483
Andrey <andreypomortsev@gmail.com>
2024-07-01 17:14:38 +0500

    Added router to loans

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
-from routers import books, customers
+from routers import books, customers, loans
 
 app = FastAPI()
 
 app.include_router(customers.router)
 app.include_router(books.router)
+app.include_router(loans.router)
