5ea6203984b49c585f20f85e4d41fe418e56e65b
Andrey <andreypomortsev@gmail.com>
2024-07-09 13:32:32 +0500

    Moved CRUD logic into app/services/customer_service.py, added users/authors to get all the authors, and changed users/ to get users who are not authors

--- app/routers/customers.py
+++ app/routers/customers.py
@@ -1,8 +1,8 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from typing import List
 from schemas.customer import CustomerCreate, CustomerUpdate, Customer
-from db.models import Customer as DBCustomer
+from services import customer_service
 from db.database import get_db
 
 router = APIRouter()
@@ -10,42 +10,24 @@ router = APIRouter()
 
 @router.post("/user/create", response_model=Customer)
 def create_user(user: CustomerCreate, db: Session = Depends(get_db)):
-    db_customer = DBCustomer(**user.dict())
-    db.add(db_customer)
-    db.commit()
-    db.refresh(db_customer)
-    return db_customer
+    return customer_service.create_user(user, db)
 
 
 @router.put("/user/{user_id}/edit", response_model=Customer)
 def update_user(user_id: int, user: CustomerUpdate, db: Session = Depends(get_db)):
-    db_customer = db.query(DBCustomer).filter(DBCustomer.id == user_id).first()
-    if not db_customer:
-        raise HTTPException(status_code=404, detail="Пользователь не найден")
-    for key, value in user.dict(exclude_unset=True).items():
-        setattr(db_customer, key, value)
-    db.commit()
-    db.refresh(db_customer)
-    return db_customer
+    return customer_service.update_user(user_id, user, db)
 
 
 @router.get("/user/{user_id}", response_model=Customer)
 def get_author_by_id(user_id: int, db: Session = Depends(get_db)):
-    db_customer = db.query(DBCustomer).filter(DBCustomer.id == user_id).first()
-    if not db_customer:
-        raise HTTPException(
-            status_code=404, detail="Не удалось найти пользователя с таким id"
-        )
-    return db_customer
+    return customer_service.get_user(user_id, db)
+
+
+@router.get("/users/authors/", response_model=List[Customer])
+def get_all_authors(skip: int = 0, limit: int = 50, db: Session = Depends(get_db)):
+    return customer_service.get_all_users(True, db, skip, limit)
 
 
 @router.get("/users/", response_model=List[Customer])
-def get_all_authors(skip: int = 0, limit: int = 50, db: Session = Depends(get_db)):
-    authors = (
-        db.query(DBCustomer)
-        .filter(DBCustomer.is_author)
-        .offset(skip)
-        .limit(limit)
-        .all()
-    )
-    return authors
+def get_all_users(skip: int = 0, limit: int = 50, db: Session = Depends(get_db)):
+    return customer_service.get_all_users(False, db, skip, limit)
