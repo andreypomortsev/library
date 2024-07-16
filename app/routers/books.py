accd3c6a485b27b41e562e35cdd0c5e29ca9e161
Andrey <andreypomortsev@gmail.com>
2024-07-16 11:57:58 +0500

    Refactored book endpoints to use  for asynchronous database operations.

--- app/routers/books.py
+++ app/routers/books.py
@@ -1,7 +1,7 @@
 from typing import List
 
 from fastapi import APIRouter, Depends
-from sqlalchemy.orm import Session
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from db.database import get_db
 from schemas.book import BookCreate, Book, BookUpdate
@@ -11,20 +11,24 @@ router = APIRouter()
 
 
 @router.post("/books/create", response_model=Book)
-def create_book(book: BookCreate, db: Session = Depends(get_db)):
-    return book_service.create_book(book, db)
+async def create_book(book: BookCreate, db: AsyncSession = Depends(get_db)):
+    return await book_service.create_book(book, db)
 
 
 @router.put("/books/{book_id}/edit", response_model=Book)
-def update_book(book_id: int, book: BookUpdate, db: Session = Depends(get_db)):
-    return book_service.update_book(book_id, book, db)
+async def update_book(
+    book_id: int, book: BookUpdate, db: AsyncSession = Depends(get_db)
+):
+    return await book_service.update_book(book_id, book, db)
 
 
 @router.get("/books/{book_id}", response_model=Book)
-def get_book_by_id(book_id: int, db: Session = Depends(get_db)):
-    return book_service.get_book(book_id, db)
+async def get_book_by_id(book_id: int, db: AsyncSession = Depends(get_db)):
+    return await book_service.get_book(book_id, db)
 
 
 @router.get("/books/", response_model=List[Book])
-def get_all_books(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
-    return book_service.get_books(db, skip, limit)
+async def get_all_books(
+    skip: int = 0, limit: int = 10, db: AsyncSession = Depends(get_db)
+):
+    return await book_service.get_books(db, skip, limit)
