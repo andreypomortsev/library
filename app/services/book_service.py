2c8f60dea3fd60e2ee61c2f04f70c6d016c5d461
Andrey <andreypomortsev@gmail.com>
2024-07-16 12:01:25 +0500

    Refactored book service functions to use  for asynchronous database operations in the FastAPI application.

--- app/services/book_service.py
+++ app/services/book_service.py
@@ -1,40 +1,48 @@
 from typing import List
 
 from fastapi import HTTPException
-from sqlalchemy.orm import Session
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from db.models import Book as DBBook
 from schemas.book import Book, BookUpdate, BookCreate
 
 
-def get_book(book_id: int, db: Session) -> Book:
-    db_book = db.query(DBBook).filter(DBBook.id == book_id).first()
+async def get_book(book_id: int, db: AsyncSession) -> Book:
+    query = select(DBBook).filter(DBBook.id == book_id)
+    result = await db.execute(query)
+    db_book = result.scalars().first()
     if not db_book:
         msg = f"There is no book with ID: {book_id}"
         raise HTTPException(status_code=404, detail=msg)
     return db_book
 
 
-def update_book(book_id: int, book_fields: BookUpdate, db: Session) -> Book:
-    book = get_book(book_id, db)
+async def update_book(
+        book_id: int, book_fields: BookUpdate, db: AsyncSession
+) -> Book:
+    book = await get_book(book_id, db)
     for key, value in book_fields.dict(exclude_unset=True).items():
         setattr(book, key, value)
-    db.commit()
-    db.refresh(book)
+    await db.commit()
+    await db.refresh(book)
     return book
 
 
-def get_books(db: Session, skip: int = 0, limit: int = 10) -> List[Book]:
-    db_books = db.query(DBBook).offset(skip).limit(limit).all()
+async def get_books(
+        db: AsyncSession, skip: int = 0, limit: int = 10
+) -> List[Book]:
+    result = await db.execute(select(DBBook).offset(skip).limit(limit))
+    db_books = result.scalars().all()
     if not db_books:
         msg = "There are no books available."
         raise HTTPException(status_code=404, detail=msg)
     return db_books
 
 
-def create_book(book: BookCreate, db: Session) -> Book:
+async def create_book(book: BookCreate, db: AsyncSession) -> Book:
     db_book = DBBook(**book.dict())
     db.add(db_book)
-    db.commit()
-    db.refresh(db_book)
+    await db.commit()
+    await db.refresh(db_book)
     return db_book
