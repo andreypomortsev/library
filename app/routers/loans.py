fcceb21f9c177de455785bc14eb6533521d96bab
Andrey <andreypomortsev@gmail.com>
2025-02-12 19:05:11 +0700

    Changed the endpoints according to the issue #20

--- app/routers/loans.py
+++ app/routers/loans.py
@@ -10,20 +10,20 @@ from services import loan_service
 router = APIRouter()
 
 
-@router.post("/loans/create", response_model=Loan)
+@router.post("/loans", response_model=Loan)
 async def create_loan(loan: LoanCreate, db: AsyncSession = Depends(get_db)):
     return await loan_service.create_loan(loan, db)
 
 
-@router.put("/loans/{book_id}/edit", response_model=Loan)
-async def update_loan(
-    book_id: int, loan: LoanUpdate, db: AsyncSession = Depends(get_db)
-):
-    return await loan_service.update_loan(book_id, loan, db)
-
-
-@router.get("/loans/", response_model=List[Loan])
+@router.get("/loans", response_model=List[Loan])
 async def get_all_loans(
     skip: int = 0, limit: int = 50, db: AsyncSession = Depends(get_db)
 ):
     return await loan_service.get_loans(db, skip, limit)
+
+
+@router.put("/loans/{book_id}", response_model=Loan)
+async def update_loan(
+    book_id: int, loan: LoanUpdate, db: AsyncSession = Depends(get_db)
+):
+    return await loan_service.update_loan(book_id, loan, db)
